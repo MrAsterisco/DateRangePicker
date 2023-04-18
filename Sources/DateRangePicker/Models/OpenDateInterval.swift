2764aa6dc042e569017d5233c9961eee576334b2
Alessio Moiso <alessio@inerziasoft.eu>
2023-04-18 20:50:48 +0200

    Add intersect method to OpenDateInterval

--- Sources/DateRangePicker/Models/OpenDateInterval.swift
+++ Sources/DateRangePicker/Models/OpenDateInterval.swift
@@ -45,6 +45,29 @@ public struct OpenDateInterval {
     return (start...end).contains(date)
   }
   
+  /// Indicates whether this interval intersects the specified interval.
+  ///
+  /// - note: An open ended interval intersects with a closed interval if its
+  /// start date is contained in the other interval.
+  ///
+  /// - Parameters:
+  ///   - interval: The interval to consider.
+  /// - Returns: `true` if the intervals intersect, `false` otherwise.
+  public func intersects(_ interval: OpenDateInterval) -> Bool {
+    guard let end else {
+      return interval.contains(start)
+    }
+    
+    guard let otherEnd = interval.end else {
+      return contains(interval.start)
+    }
+    
+    let this = DateInterval(start: start, end: end)
+    let other = DateInterval(start: interval.start, end: otherEnd)
+    
+    return this.intersects(other)
+  }
+  
   /// Get the duration of the interval.
   /// 
   /// - note: An open-ended interval will always have a duration of `0`.
